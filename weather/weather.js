ed64dd236a28b0bc552569b6b24436b6c0bee4ff
andreyb622 <andreyb622@gmail.com>
2021-01-13 20:39:22 +0300

    add select tag

--- weather/weather.js
+++ weather/weather.js
@@ -1,10 +1,12 @@
 const weatherIcon = document.querySelector('.weather-icon');
 const temperature = document.querySelector('.temperature');
 const weatherDescription = document.querySelector('.weather-description');
-const city = document.querySelector('.city');
+
+const city = document.getElementById('cities')
+const getWeatherButton = document.querySelector('.button')
 
 async function getWeather() {
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.textContent}&lang=ru&appid=ded1d7fe2e2f677e43611b3e0fe25e22&units=metric`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&lang=ru&appid=ded1d7fe2e2f677e43611b3e0fe25e22&units=metric`;
   const res = await fetch(url);
   const data = await res.json();
 
@@ -13,28 +15,6 @@ async function getWeather() {
   weatherDescription.textContent = data.weather[0].description;
 }
 
-function getCity() {
-  if (localStorage.getItem('city') === null) {
-    city.textContent = 'Минск';
-  } else {
-    city.textContent = localStorage.getItem('city');
-  }
-  getWeather();
-}
+getWeather()
 
-function setCity(e) {
-  if (e.type === 'keypress') {
-    if (e.which == 13 || e.keyCode == 13) {
-      localStorage.setItem('city', e.target.innerText);
-      city.blur();
-    }
-  } else {
-    localStorage.setItem('city', e.target.innerText);
-  }
-  getWeather();
-}
-
-getCity();
-
-document.addEventListener('DOMContentLoaded', getWeather);
-city.addEventListener('keypress', setCity);
+getWeatherButton.addEventListener('click', getWeather)
