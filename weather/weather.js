2af273419679adffd4e49247311cd69158bebf8f
andreyb622 <andreyb622@gmail.com>
2021-01-20 22:39:03 +0300

    add all cities of BY

--- weather/weather.js
+++ weather/weather.js
@@ -6,9 +6,11 @@ const city = document.getElementById('cities')
 const getWeatherButton = document.querySelector('.button')
 
 async function getWeather() {
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&lang=ru&appid=ded1d7fe2e2f677e43611b3e0fe25e22&units=metric`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city.value}&lang=ru&appid=fdaed127dd07d0eca0c2e4345b0d194d&units=metric`;
   const res = await fetch(url);
   const data = await res.json();
+
+  console.log(data)
 
   weatherIcon.classList.add(`owf-${data.weather[0].id}`);
   temperature.textContent = `${Math.round(data.main.temp)}°C`;
